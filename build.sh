dcb4bcce9cc6ee3aa9ac9d0b43a0323bd06a8c42
Andrey Malov <ndrmlv@ya.ru>
2022-06-03 13:07:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-clang++ src/main.cpp src/lib.cpp -o bot	\
+g++ src/main.cpp src/lib.cpp -o bot	\
     --std=c++17 -I/usr/local/include -Wall -Wextra \
     -lTgBot -lboost_system -lssl -lcrypto -lpthread \
     -O2
